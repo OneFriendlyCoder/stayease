d4e3a042e589a1714b1ef48595be620e640c0a97
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-11 16:41:31 +0530

    image upload cloudinary cdn

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 enum STEPS {
     CATEGORY = 0,
     LOCATION = 1,
@@ -39,7 +40,8 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount= watch('roomCount');
     const bathroomCount = watch('bathroomCount');
-
+    const imageSrc = watch('imageSrc');
+    
     const setCustomValue = (id: string, value: any) =>{
         setValue(id, value, {
             shouldValidate: true,
@@ -106,6 +108,16 @@ const RentModal = () => {
                 <Counter title="Rooms" subtitle="How many rooms do you have ?" value={roomCount} onChange={(value) => setCustomValue('roomCount', value)}/>
                 <hr />
                 <Counter title="Bathrooms" subtitle="How many bathrooms do you provide" value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)}/>
+            </div>
+            
+        )
+    }
+
+    if(step === STEPS.IMAGES){
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading title="Add a photo of your place" subtitle="Show guests what your place looks like"/>
+                <ImageUpload value={imageSrc} onChange={(value) => setCustomValue('imageSrc', value)}/>
             </div>
             
         )
